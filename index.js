6b9aa9bab32538dea4e28d5219049e641c96fa84
alfa96git <alfa.sunny.96@gmail.com>
2023-02-16 04:41:39 +0600

    First Card add properly

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log("added")
+// Coffee Js - first Card
+document.getElementById('btn-coffee-card').addEventListener('click', function () {
+
+    const cardName = document.getElementById('coffee-card-Name').innerText;
+    const cardPrice = document.getElementById('coffee-Price').innerText;
+
+    const cardQuantity = document.getElementById('coffee-Quantity').innerText;
+
+    const totalPrice = parseInt(cardPrice) / parseInt(cardQuantity);
+    console.log(cardName, cardPrice, cardQuantity, totalPrice);
+
+    // Take Data From Card & Display on table
+    const ProductValueContainer = document.getElementById('table-container');
+
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${1}</td>
+    <td>${cardName}</td>
+    <td>${cardPrice}</td>
+    <td>${cardQuantity}</td>
+    <td>${totalPrice}</td>
+
+    <td>
+    <button class="btn btn--sm btn-red-400"> Double </button>
+    </td>
+
+    `;
+
+    ProductValueContainer.appendChild(tr);
+
+})
